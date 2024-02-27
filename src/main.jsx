9c0f3d3cbeff081c7e93f97d2a18f3c97ee65a04
Mustafa Guleroglu <must60yyy@gmail.com>
2024-02-28 02:47:15 +0300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
-import dotenv from "dotenv";
-dotenv.config();
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
